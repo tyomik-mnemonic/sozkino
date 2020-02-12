a9d1531ad4bc229b9a5ad79eb9b80818774202dd
tyomik-mnemonic <42242593+tyomik-mnemonic@users.noreply.github.com>
2020-02-12 18:46:54 +0300

    add frame of movie model

--- kino/models.py
+++ kino/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
+
+#class SozMovie(models.Model):
+#    id = models.IntegerField(primary_key=True)
+#    original_title = models.TextField()
+#    budget = models.IntegerField()
+#    popularity = models.FloatField()
+#    release_date = models.DataField()
+#    vote_average = models.FloatField()
+#    original_language = models.TextField()
+
+#    class Meta:
+#        managed = True
+#        verbose_name = 'фильм'
+#        verbose_name = 'фильмы'
+        
 # Create your models here.
